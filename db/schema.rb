0bef4eb0eea76111ba03b57c11473e089a78d781
Jim Schultz <boxenjim@gmail.com>
2014-12-09 11:51:48 -0800

    getting idea model and tests setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209194013) do
+ActiveRecord::Schema.define(version: 20141209194803) do
 
   create_table "ideas", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 describe Idea do
 
   context 'with new idea instance' do
+    let(:idea) { Idea.new(description: 'anIdea') }
 
-  it 'should exist' do
-    expect(idea).to be_valid
+    it 'should exist' do
+      expect(idea).to be_valid
+    end
+
+    it 'should have description column' do
+      expect(idea.description).to eq 'anIdea'
+    end
   end
 
-  end
+  
+
 end
