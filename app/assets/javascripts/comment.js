dc50ee4614e2d4da7534a586ccd89a3f71c055b2
Jim Schultz <boxenjim@gmail.com>
2014-12-17 09:59:17 -0800

    clears comment form on success

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#comment_form').on('ajax:success',function(xhr, data, status){
-    console.log(data)
-    $("ul#comments").append("<li>" + data.content + "</li>")
+    $("ul#comments").append("<li>" + data.content + "</li>");
+    $("#comment_form").trigger("reset");
   }).on('ajax:error',function(xhr, status, error){
     alert("Oops");
   });
